65a9da517d6cacee2188bda039ebfb6173ec0645
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-09-30 17:10:25 +0200

    fix the code:
    - add a condition to validate the data
    - modify the conditions for discount

--- script.js
+++ script.js
@@ -9,57 +9,59 @@
 // first of all we have to ask the data from the user
 
 // Ask the km
-const km = prompt('Inserisci qui la distanza in Km del tuo viaggio');
+const km = prompt('Inserisci qui i Km del tuo viaggio');
 // now we have to converte km string to a number
 const numberKm = Number(km);
-// stamp the km in console
-const yourKm = `Km: ${numberKm}`;
-console.log(yourKm);
 
 // Ask the age
 const age = prompt('Inserisci qui la tua età');
 // now we have to converte age string to a number
 const numberAge = Number(age);
-// stamp the age in console
-const yourAge = `Età: ${age}`;
-console.log(yourAge);
 
+// Condition to validate the data
+if (numberKm > 0 && numberAge > 0) {
 
+    // stamp the km in console
+    const yourKm = `Km: ${numberKm}`;
+    console.log(yourKm);
+    
+    // stamp the age in console
+    const yourAge = `Età: ${age}`;
+    console.log(yourAge);
+    
+    // Now set up the rules to find out the final price
+    
+    // First rule: find the ticket price
+    let costKm = (numberKm * 0.21).toFixed(2) ;
+    // stamp the final price without discount
+    console.log(`Prezzo totale: ${costKm}€`);
+    
+    // Conditions for access to discount
+    // Second rule: if the user age is minor than 18, apply a discount to costKm
+    if (numberAge < 18) {
+        const discountAmount = (costKm * 20 /100).toFixed(2);
+        const priceDiscounted = costKm - discountAmount;
+        // stamp discount
+        console.log(`Sconto applicato: -${discountAmount}€`);
+        // fix the cost in 2 decimal
+        const finalPrice = priceDiscounted.toFixed(2);
+        // stamp the final price discounted
+        console.log(`Prezzo totale scontato: ${finalPrice}€`);
+    } 
+    // Third rule: if the user age is higher than 65, apply a discount to costKm
+    else if (numberAge > 65){
+        const discountAmount = (costKm * 40 /100).toFixed(2);
+        const priceDiscounted = costKm - discountAmount;
+        // stamp discount
+        console.log(`Sconto applicato: -${discountAmount}€`);
+        // fix the cost in 2 decimal
+        const finalPrice = priceDiscounted.toFixed(2);
+        // stamp the final price discounted
+        console.log(`Prezzo totale scontato: ${finalPrice}€`);
+    } 
 
-// Now set up the rules to find out the final price
-
-// First rule: find the ticket price
-let costKm = numberKm * 0.21 ;
-
-// Second rule: if the user age is minor than 18, apply a discount to costKm
-if (numberAge < 18) {
-    const costKmDiscounted = costKm - costKm * 20 /100 ;
-    // fix the cost in 2 decimal
-    const costKmDiscountedTwoDecimals = costKmDiscounted.toFixed(2);
-    // tranform this cost in Euro
-    const costKmDiscountedTwoDecimalsEuro = `${costKmDiscountedTwoDecimals} €`;
-    // stamp the final price in euro rounded from this statement 
-    const finalPrice = `Prezzo totale: ${costKmDiscountedTwoDecimalsEuro}`;
-    console.log(finalPrice);
-} else if (18 <= numberAge && numberAge <= 65){
-    // fix the cost in 2 decimal
-    const costKmTwoDecimals = costKm.toFixed(2);
-    // tranform this cost in Euro
-    const costKmTwoDecimalsEuro = `${costKmTwoDecimals} €`;
-    // stamp the final price in euro rounded from this statement 
-    const finalPrice = `Prezzo totale: ${costKmTwoDecimalsEuro}`;
-    console.log(finalPrice);
+} else{
+    console.log('Dati non validi'); 
 }
-// Third rule: if the user age is higher than 65, apply a discount to costKm
-else {
-    const costKmDiscounted = costKm - costKm * 40 /100 ;
-    // fix the cost in 2 decimal
-    const costKmDiscountedTwoDecimals = costKmDiscounted.toFixed(2) ;
-    // tranform this cost in Euro
-    const costKmDiscountedTwoDecimalsEuro = `${costKmDiscountedTwoDecimals} €` ;
-    // stamp the final price in euro rounded from this statement  
-    const finalPrice = `Prezzo totale: ${costKmDiscountedTwoDecimalsEuro}` ;
-    console.log(finalPrice);
-} 
 
 
