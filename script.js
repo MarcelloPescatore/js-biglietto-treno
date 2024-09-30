4e7e888e89876a6f35e7f8a0aaf49afe99f3b719
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-09-30 16:06:17 +0200

    analize the assignment and set up the steps

--- script.js
+++ script.js
@@ -6,7 +6,50 @@
 // L'output del prezzo finale va messo fuori in forma umana
 
 
+// first of all we have to ask the data from the user
 
+// Ask the km
+
+// now we have to converte km string to a number
+
+// stamp the km in console
+
+
+// Ask the age
+
+// now we have to converte age string to a number
+
+// stamp the age in console
+
+
+
+
+// Now set up the rules to find out the final price
+
+// First rule: find the ticket price
+
+
+// Second rule: if the user age is minor than 18, apply a discount to costKm
+if (numberAge < 18) {
+    
+    // fix the cost in 2 decimal
+    
+    // tranform this cost in Euro
+    
+    // stamp the final price in euro rounded from this statement 
+    
+}
+
+// Third rule: if the user age is higher than 65, apply a discount to costKm
+if (numberAge > 65) {
+   
+    // fix the cost in 2 decimal
+    
+    // tranform this cost in Euro
+    
+    // stamp the final price in euro rounded from this statement  
+    
+}
 
 
 
